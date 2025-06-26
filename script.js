0a0189a0ab874d08d553ec0d2e4f3159f924e155
Farouk <74254555+MohamedFarouk94@users.noreply.github.com>
2025-06-26 14:10:25 +0300

    Update script.js fixing phone nav

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const body = document.body
 
 const btnTheme = document.querySelector('.fa-moon')
-const btnHamburger = document.querySelector('.fa-bars')
+const btnHamburger = document.querySelector('.nav__hamburger')
 
 const addThemeClass = (bodyClass, btnClass) => {
   body.classList.add(bodyClass)
@@ -33,14 +33,15 @@ btnTheme.addEventListener('click', toggleTheme)
 
 const displayList = () => {
 	const navUl = document.querySelector('.nav__list')
+	const icon = btnHamburger.querySelector('i')
 
-	if (btnHamburger.classList.contains('fa-bars')) {
-		btnHamburger.classList.remove('fa-bars')
-		btnHamburger.classList.add('fa-times')
+	if (icon.classList.contains('fa-bars')) {
+		icon.classList.remove('fa-bars')
+		icon.classList.add('fa-times')
 		navUl.classList.add('display-nav-list')
 	} else {
-		btnHamburger.classList.remove('fa-times')
-		btnHamburger.classList.add('fa-bars')
+		icon.classList.remove('fa-times')
+		icon.classList.add('fa-bars')
 		navUl.classList.remove('display-nav-list')
 	}
 }
